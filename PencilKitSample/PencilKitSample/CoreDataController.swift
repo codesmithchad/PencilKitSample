57d55d3d8043acf35885be3f2303b6a0a6bf141e
Hyuck Hur <ajiaco@stunitas.com>
2022-02-08 15:10:05 +0900

    add delete

--- PencilKitSample/PencilKitSample/CoreDataController.swift
+++ PencilKitSample/PencilKitSample/CoreDataController.swift
@@ -4,6 +4,7 @@
 //
 //  Created by Ajiaco on 2022/02/07.
 //
+//  more ref: https://zeddios.tistory.com/989
 
 import Foundation
 import CoreData
@@ -18,16 +19,18 @@ final class CoreDataController {
     }
     
     struct WritingModel {
-        var createdAt: Date
-        var scribble: Data?
-        var scribbleType: ScribbleType?
         var title: String?
+        var createdAt: Date
+        var pageNo: Int
+        var scribbleType: ScribbleType?
+        var scribble: Data?
         
-        init(createdAt: Date = Date(), scribble: Data? = nil, scribbleType: ScribbleType = .note, title: String? = nil) {
-            self.createdAt = createdAt
-            self.scribble = scribble
-            self.scribbleType = scribbleType
+        init(title: String? = nil, createdAt: Date = Date(), pageNo: Int = 0, scribbleType: ScribbleType = .note, scribble: Data? = nil) {
             self.title = title
+            self.createdAt = createdAt
+            self.pageNo = pageNo
+            self.scribbleType = scribbleType
+            self.scribble = scribble
         }
     }
     
@@ -43,16 +46,19 @@ final class CoreDataController {
         return container
     }()
     
+    private lazy var context = persistentContainer.viewContext
+    
     // MARK: - Core Data Saving support
     
     func save(_ writingModel: WritingModel, completion: @escaping () -> Void) {
-        let context = persistentContainer.viewContext
+//        let context = persistentContainer.viewContext
         guard let entity = NSEntityDescription.entity(forEntityName: entityName, in: context) else { return }
         let wiring = NSManagedObject(entity: entity, insertInto: context)
-        wiring.setValue(writingModel.createdAt, forKey: "createdAt")
-        wiring.setValue(writingModel.scribble, forKey: "scribble")
-        wiring.setValue(0, forKey: "scribbleType")
         wiring.setValue(writingModel.title, forKey: "title")
+        wiring.setValue(writingModel.createdAt, forKey: "createdAt")
+        wiring.setValue(writingModel.pageNo, forKey: "pageNo")
+        wiring.setValue(writingModel.scribbleType?.rawValue, forKey: "scribbleType")
+        wiring.setValue(writingModel.scribble, forKey: "scribble")
 
         guard context.hasChanges else { return }
 
@@ -65,7 +71,7 @@ final class CoreDataController {
     }
     
     func fetch() {
-        let context = persistentContainer.viewContext
+//        let context = persistentContainer.viewContext
         let fetchRequest = NSFetchRequest<NSManagedObject>(entityName: entityName)
 //        fetchRequest.predicate = NSPredicate(format: "scribbleType = %d", 0)
         do {
@@ -77,6 +83,15 @@ final class CoreDataController {
 //                                    scribbleType: $0.value(forKey: "scribbleType") as? Int ?? 0,
 //                                    title: $0.value(forKey: "title") as? String)
 //            })
+        } catch let error as NSError {
+            fatalError("Unresolved error \(error), \(error.userInfo)")
+        }
+    }
+    
+    func delete(_ index: Int) {
+        context.delete(writings[index])
+        do {
+            try context.save()
         } catch let error as NSError {
             fatalError("Unresolved error \(error), \(error.userInfo)")
         }
